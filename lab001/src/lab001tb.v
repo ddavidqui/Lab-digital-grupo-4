d7178ac985f3f5017f6cafe66f3bda169c5be2b7
ddavidqui <cporrasq@unal.edu.co>
2024-03-12 22:50:20 -0500

    axtualiuz

--- lab001/src/lab001tb.v
+++ lab001/src/lab001tb.v
@@ -1,4 +1,4 @@
-`include "lab001/lab001.v" // Se llama el .v donde va la logica
+`include "lab001.v" // Se llama el .v donde va la logica
 `timescale 1ps/1ps 
 
 module lab001tb(); //EL modulo lo llamas como quieras aca lo llame igual que el archivo
@@ -6,7 +6,6 @@ module lab001tb(); //EL modulo lo llamas como quieras aca lo llame igual que el 
 reg A_tb; // Entradas de tu logica del archivo anterior
 reg B_tb; // mi segunda entrada
 reg Ci_tb; // Tercera entrada
-
 wire S_; //ESTAN SON LAS SALIDAS
 wire Co_; //SALIDAS
 
